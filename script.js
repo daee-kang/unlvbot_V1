9248e3953c8a2b1eb9aca2c60580321a443550b3
daee kang <44410043+daeeKang@users.noreply.github.com>
2019-05-24 00:55:38 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var idCounter = 0;
-
-function addClass(){
-    idCounter++;
-    var html = "<input type='text' id='class" + idCounter +"><input type='text' id='classNum" + idCounter +">";
-    document.getElementById('classes').innerHTML += html;
-}
